7aec04e0ac55c28d879c320e5b0b6a306f06a27e
elio0 <rodriguezeliomar088@gmail.com>
2022-10-10 21:29:14 +0200

    Modifica classe GestioneGioco && GestioneJson
    Nella classe GestioneGioco viene aggiunto:
    GestioneGioco.controlloPartitaFinita()
    <Restituisce un boolean, true se è finita, false se non lo è>
    
    GestioneGioco.prendereIdGiocatoreCasella()
    <Restituisce l'id del giocatore ch'è nella casella dove arriva il nuovo giocatore>
    
    GestioneGioco.stampaDescrizioneCasellaSpeciale()
    <Restituisce la descrizione se un giocatore è arrivato in una casella speciale>
    
    GestioneGioco.verificaCasellaLibera()
    <Verifica se nella casella che si deve arrivare c'è alcun giocatore>
    
    Nella classe GestioneGioco viene modificato:
    GestioneGioco.spostareGiocatore()
    <Si aggiungono ulteriori controlli per la partita>
    
    Nella classe GestioneJson viene modificato:
    <Molti metodi che per questo programma sarebbero inutili>
    
    10/10/2022 ore: 20:00

--- src/GestioneGioco.java
+++ src/GestioneGioco.java
@@ -1,4 +1,5 @@
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -41,13 +42,97 @@ public class GestioneGioco {
         }
     }
 
-    public void andareAvantiCasella(int risultatoDado, int casellaAttualeGiocatore){
+    public void spostareGiocatore(int id, int risultatoDado, int casellaAttualeGiocatore, int numeroCaselle){
         GestioneJson gestioneJson = new GestioneJson(directory);
 
-        JSONArray caselleArray = gestioneJson.totaleProdotti(caselle);
+        JSONArray caselleArray = gestioneJson.totaleCaselle(caselle);
 
         int casellaAggiornata = risultatoDado + casellaAttualeGiocatore;
 
-        System.out.println(caselleArray.get(casellaAggiornata - 1));
+        if (casellaAggiornata > numeroCaselle)
+            casellaAggiornata = casellaAggiornata - numeroCaselle;
+
+        for (Object o : caselleArray) {
+            JSONObject casellaCapitata = (JSONObject) o;
+
+            if (casellaAggiornata == (int) casellaCapitata.get("idCasella")) {
+                if ((boolean) casellaCapitata.get("vaiAvanti"))
+                    casellaAggiornata = casellaAggiornata + (int) casellaCapitata.get("caselleDaMuoversi");
+
+                else
+                    casellaAggiornata = casellaAggiornata - (int) casellaCapitata.get("caselleDaMuoversi");
+
+                if (casellaAggiornata > numeroCaselle)
+                    casellaAggiornata = casellaAggiornata - numeroCaselle;
+
+            }
+        }
+
+        if (casellaAggiornata != numeroCaselle) {
+            if (!verificaCasellaLibera(casellaAggiornata)) {
+                giocatoreArrayList.get(id).setPosizioneGiocatore(casellaAggiornata);
+
+            } else {
+                giocatoreArrayList.get(prendereIdGiocatoreCasella(casellaAttualeGiocatore)).setPosizioneGiocatore(casellaAttualeGiocatore);
+                giocatoreArrayList.get(id).setPosizioneGiocatore(casellaAggiornata);
+            }
+
+        }else
+            System.out.println("Il giocatore N^" + id + " Ha vinto la partita!!\n" + "Congratulazioni!!!");
+    }
+
+    public boolean controlloPartitaFinita(int numeroCaselle){
+        boolean partitaFinita = false;
+
+        for (Giocatore giocatore : giocatoreArrayList) {
+            if (giocatore.getPosizioneGiocatore() == numeroCaselle) {
+                partitaFinita = true;
+                break;
+            }
+        }
+
+        return partitaFinita;
+    }
+
+    public String stampaDescrizioneCasellaSpeciale(int casellaCorrente){
+        String descrizioneCasella = "";
+        GestioneJson gestioneJson = new GestioneJson(directory);
+
+        JSONArray caselleArray = gestioneJson.totaleCaselle(caselle);
+
+        for (Object o : caselleArray) {
+            JSONObject caselleSpeciali = (JSONObject) o;
+
+            if (casellaCorrente == (int) caselleSpeciali.get("idCasella"))
+                descrizioneCasella = (String) caselleSpeciali.get("descrizioneCasella");
+
+        }
+
+        return descrizioneCasella;
+    }
+
+    public boolean verificaCasellaLibera(int posizioneGiocatore){
+        boolean giocatoreTrovato = false;
+
+        for (Giocatore giocatore : giocatoreArrayList) {
+            if (posizioneGiocatore == giocatore.getPosizioneGiocatore()) {
+                giocatoreTrovato = true;
+                break;
+            }
+        }
+
+        return giocatoreTrovato;
+    }
+
+    public int prendereIdGiocatoreCasella(int posizioneGiocatore){
+        int idGiocatoreCasella = 0;
+
+        for (Giocatore giocatore : giocatoreArrayList) {
+            if (posizioneGiocatore == giocatore.getPosizioneGiocatore()) {
+                 idGiocatoreCasella = giocatore.getId();
+            }
+        }
+
+        return idGiocatoreCasella;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         GestioneGioco gestioneGioco = new GestioneGioco();
 
-        gestioneGioco.andareAvantiCasella(1, 0);
     }
 }

dea58da562d4d2b51ac3d8dac72bd7e82d18e1a9
elio0 <rodriguezeliomar088@gmail.com>
2022-10-12 10:30:50 +0200

    UWU 2
    Salva Partita

--- src/GestioneJson.java
+++ src/GestioneJson.java
@@ -1,12 +1,18 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GestioneJson {
     public String directory;
+
+    private String[] tipiOggetti = new String[4];
 
     /**
      * Metodo costruttore(), che inizializza tutte le variabili
@@ -14,6 +20,10 @@ public class GestioneJson {
      */
     public GestioneJson(String directory){
         this.directory = directory;
+        this.tipiOggetti[0] = "id";
+        this.tipiOggetti[1] = "posizioneGiocatore";
+        this.tipiOggetti[2] = "rimanereFermo";
+        this.tipiOggetti[3] = "rimanereBloccato";
     }
 
     /**
@@ -35,6 +45,54 @@ public class GestioneJson {
         } catch (IOException | org.json.simple.parser.ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Metodo aggiungereProdottoJson(), che aggiunge dei prodotti al file json
+     * @param giocatoreArrayList richiede un'Array di json con tutti gli oggetti dentro
+     * @throws IOException eccezione
+     * @throws ParseException eccezione parse
+     */
+    public void salvarePartitaJson(ArrayList<Giocatore> giocatoreArrayList) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            FileReader fileReader = new FileReader(directory);
+
+            Object obj = jsonParser.parse(fileReader); //Oggetto con l'informazione del file json
+
+            JSONObject jsonObject = (JSONObject) obj; //Oggetto di tipo Json con l'informazione dell'oggetto precedente
+
+            JSONArray arrayOggettoSalvare = (JSONArray) jsonObject.get("partite"); //Array di oggetti json
+
+            for (int i = 0; i < giocatoreArrayList.size(); i++) { //For-each che prende ogni oggetto e lo salva su una variabile temporanea
+                JSONObject oggettoSalvare = new JSONObject();
+
+                Object[] infoConvertita = new Object[5];
+
+                //Converto le stringhe nei tipi di variabili utilizzati prima di salvarlo nel file json
+                infoConvertita[0] = giocatoreArrayList.get(i).getId();
+                infoConvertita[1] = giocatoreArrayList.get(i).getPosizioneGiocatore();
+                infoConvertita[2] = giocatoreArrayList.get(i).getRimanereFermo();
+                infoConvertita[3] = giocatoreArrayList.get(i).isRimanereBloccato();
+
+                for (int j = 0; j < tipiOggetti.length; j++) //For che salva l'informazione dentro l'oggetto json
+                    oggettoSalvare.put(tipiOggetti[j] + i, infoConvertita[j]);
+
+
+                arrayOggettoSalvare.add(oggettoSalvare);//Inserisco l'oggetto json dentro l'Array
+
+            }
+
+            jsonObject.put("partite", arrayOggettoSalvare);//Inserisco l'Array di jsonObject dentro un oggetto
+
+            FileWriter file = new FileWriter(directory);
+
+            file.write(jsonObject.toJSONString());//Scrive l'oggetto dentro il file json
+            file.flush();
+            file.close();//Chiude il fileWriter
+        } catch (FileNotFoundException ignored){}
+
     }
 
 }
